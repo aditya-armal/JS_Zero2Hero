c5d5faea9de7e109295944934285d8ec9a1c392c
Aditya Armal <aditya.armal@srivisifai.in>
2023-10-30 01:08:10 +0530

    Vairables log in JS

---
+++ Day8/script.js
@@ -0,0 +1,8 @@
+let firstName = 'Aditya';
+let age = 25;
+
+console.log(`My name is
+${firstName}
+and I am ${age} years old`);
+
+console.log('My name is \n' + firstName + '\n and I am ' + age + ' years old');
